90fd27992146d5f911a0131cf3a49c6b8428b7c1
geetha <gees.elearnings@gmail.com>
2019-04-05 20:25:06 -0700

    104- git ignore concept

--- HiWorld.java
+++ HiWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class HiWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
