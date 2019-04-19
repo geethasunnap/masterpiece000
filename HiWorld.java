49a6dad02f6b8f605ccf7c92c06071e7e440f146
geetha <gees.elearnings@gmail.com>
2019-04-18 18:28:07 -0700

    new change in hf

--- HiWorld.java
+++ HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
+      Prints "Hello, World" in the terminal window.
       System.out.println("My, Session");
       System.out.println("My first class");
    }
