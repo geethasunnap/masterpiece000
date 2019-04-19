2eb593934c9834313406042e82f4ee216db1e00a
geetha <gees.elearnings@gmail.com>
2019-04-18 18:54:00 -0700

    added code

--- HiWorld.java
+++ HiWorld.java
@@ -1,7 +1,6 @@
 public class HiWorld {
    public static void main(String[] args) {
-      Prints "Hello, World" in the terminal window.
-      System.out.println("My, Session");
+      Sysut.println("My, Session");
       System.out.println("My first class");
    }
 }
